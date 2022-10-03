8c3db74bfbe9f2948060deae7e3224041d748f88
Jack <rjacksonhelmer@gmail.com>
2022-10-03 10:05:31 -0400

    initial re-commit

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
 
 
