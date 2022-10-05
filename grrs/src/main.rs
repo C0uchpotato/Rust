f7de974516df5067a30514b5fb6507f8d58bce48
Jack <rjacksonhelmer@gmail.com>
2022-10-05 09:46:58 -0400

    Unit test (from docs)

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,11 @@
 #![allow(unused)]
 
+
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
 use clap::Parser;
+use anyhow::{Context, Result};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -11,13 +16,26 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let result = std::fs::read_to_string("test.txt");
-    let content = match result {
-        Ok(content) => { content },
-        Err(error) => { return Err(error.into()); }
-    };
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
     println!("file content: {}", content);
+    Ok(())
+}
+
+
+
+//Testing
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
     Ok(())
 }
