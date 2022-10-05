14817e917c355b46cad3b0fce69adff43078b3ac
Jack <rjacksonhelmer@gmail.com>
2022-10-05 12:45:37 -0400

    Restructure tests

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,8 +1,4 @@
 #![allow(unused)]
-
-
-use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
 use clap::Parser;
 use anyhow::{Context, Result};
@@ -28,14 +24,4 @@ fn main() -> Result<()> {
 
 //Testing
 
-#[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("grrs")?;
 
-    cmd.arg("foobar").arg("test/file/doesnt/exist");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("could not read file"));
-
-    Ok(())
-}
